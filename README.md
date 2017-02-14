a53950c118e63d8b7a57e163041b5b9eda7305dd
José Tiago <josetiagobsouza@gmail.com>
2017-02-14 15:28:43 -0200

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Ionic2 + Firebase Start 
+
+git clone https://github.com/josetiagobispo/ionic2StartFirebase.git
+
+npm install
+
+ionic serve
---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { AngularFireModule } from 'angularfire2';
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+
+//GET TO FIREBASE WEBSITE
+export const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  storageBucket: "",
+  messagingSenderId: ""
+};
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage
+  ],
+  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+})
+export class AppModule {}
